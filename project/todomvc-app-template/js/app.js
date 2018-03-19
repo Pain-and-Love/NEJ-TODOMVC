1aee19542c34990cdacff694ed1b0d5c5168547e
hbsz-01 <506868247@qq.com>
2018-03-19 22:23:13 +0800

    start

--- project/todomvc-app-template/js/app.js
+++ project/todomvc-app-template/js/app.js
@@ -1,6 +1,21 @@
-(function (window) {
-	'use strict';
+function add2List(value, ulNode) {
+  console.log('将', value, '拼接成li | ', 'append到', ulNode);
+  var newLi = ulNode._$create('li');
+  var innerHTML = '<div class="view"><input class="toggle" type="checkbox"><label>' + value + '</label><button class="destroy"></button></div><input class="edit" value="' + value + '">';
+  newLi.innerHTML = innerHTML;
+  ulNode._$insert(newLi);
+}
 
-	// Your starting point. Enjoy the ride!
-
-})(window);
+NEJ.define([
+  'util/chain/chainable',
+  'base/element'
+], function($, _e) {
+  $('#todo-input')._$addEvent('keyup', function(_event) {
+    // var val = _event.target.value;
+    var val = $(this)._$val();
+    if(_event.keyCode === 13) {
+      // 提交到list
+      add2List(val, $('#todo-list'))
+    }
+  })
+});

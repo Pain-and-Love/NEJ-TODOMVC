e4910fa7ce1cd440003d76d65960768150620ce4
XiaFan <506868247@qq.com>
2018-03-20 02:36:58 +0800

    先确定思路！！

--- project/todomvc-app-template/js/app.js
+++ project/todomvc-app-template/js/app.js
@@ -1,21 +1,53 @@
-function add2List(value, ulNode) {
-  console.log('将', value, '拼接成li | ', 'append到', ulNode);
-  var newLi = ulNode._$create('li');
-  var innerHTML = '<div class="view"><input class="toggle" type="checkbox"><label>' + value + '</label><button class="destroy"></button></div><input class="edit" value="' + value + '">';
-  newLi.innerHTML = innerHTML;
-  ulNode._$insert(newLi);
-}
-
 NEJ.define([
-  'util/chain/chainable',
-  'base/element'
+	'util/chain/chainable',
+	'base/element'
 ], function($, _e) {
-  $('#todo-input')._$addEvent('keyup', function(_event) {
-    // var val = _event.target.value;
-    var val = $(this)._$val();
-    if(_event.keyCode === 13) {
-      // 提交到list
-      add2List(val, $('#todo-list'))
-    }
-  })
+	var todoList = [];
+	var listType = 'all';
+
+	function render() {
+		//  根据type和list来渲染列表
+	}
+
+	// 增加
+	function addItem(value) {
+		todoList.push({
+			id: Math.random(),
+			value: value,
+			isEditing: false,
+			isCompleted: false
+		});
+		render();
+
+		// var newLi = _e._$create('li');
+		// var innerHTML = '<div class="view"><input class="toggle" type="checkbox"><label>' + value + '</label><button class="destroy"></button></div><input class="edit" value="' + value + '">';
+		// newLi.innerHTML = innerHTML;
+		// ulNode._$insert(newLi);
+	}
+
+	// 删除
+	function removeItem(index) {
+		todoList.splice(index, 1);
+		render();
+		// _e._$remove(liNode, false);
+	}
+
+	$('#todo-list')._$addEvent('click', function(_event) {
+		var _$dom = $(_event.target);
+		var _$li = _$dom._$parent('li');
+		switch(_$dom._$attr('class')) {
+			case 'destroy':
+				// 删除对应_$li序号
+				var index = 1;
+				removeItem(index);
+				break;
+		}
+	})
+	$('#todo-input')._$addEvent('keyup', function(_event) {
+		var val = $(this)._$val().trim();
+		if(_event.keyCode === 13 && val) {
+			addItem(val);
+			$(this)._$val('');
+		}
+	})
 });

a29152700d58a84b619415bdf62c1db2961a02a2
XiaFan <506868247@qq.com>
2018-03-20 22:26:07 +0800

    silu

--- project/todomvc-app-template/js/app.js
+++ project/todomvc-app-template/js/app.js
@@ -1,53 +1,64 @@
 NEJ.define([
-	'util/chain/chainable',
-	'base/element'
-], function($, _e) {
-	var todoList = [];
-	var listType = 'all';
+  'util/chain/chainable',
+  'base/element',
+  'base/util'
+], function($, _e, _u) {
+  var todoList = [];
+  var listType = 'all';
 
-	function render() {
-		//  根据type和list来渲染列表
-	}
+  function render() {
+    //  根据type和list来渲染列表
+    var ulInnerHTML = '';
+    _u._$forEach(todoList, function(_item, _index) {
+      ulInnerHTML += '<li data-index=' + _index + '>' +
+        '<div class="view">' +
+        '<input class="toggle" type="checkbox">' +
+        '<label>' + _item.value + '</label>' +
+        '<button class="destroy"></button>' +
+        '</div>' +
+        '<input class="edit" value="' + _item.value + '">' +
+        '</li>';
+    });
+    renderFooter();
+    $('#todo-list')._$html(ulInnerHTML);
+  }
 
-	// 增加
-	function addItem(value) {
-		todoList.push({
-			id: Math.random(),
-			value: value,
-			isEditing: false,
-			isCompleted: false
-		});
-		render();
+  function renderFooter() {
+    $('#footer')._$style('display', todoList.length ? 'block' : 'none');
+  }
 
-		// var newLi = _e._$create('li');
-		// var innerHTML = '<div class="view"><input class="toggle" type="checkbox"><label>' + value + '</label><button class="destroy"></button></div><input class="edit" value="' + value + '">';
-		// newLi.innerHTML = innerHTML;
-		// ulNode._$insert(newLi);
-	}
+  // 增加
+  function addItem(value) {
+    todoList.push({
+      id: Math.random(),
+      value: value,
+      isEditing: false,
+      isCompleted: false
+    });
+    render();
+  }
 
-	// 删除
-	function removeItem(index) {
-		todoList.splice(index, 1);
-		render();
-		// _e._$remove(liNode, false);
-	}
+  // 删除
+  function removeItem(index) {
+    todoList.splice(index, 1);
+    render();
+    // _e._$remove(liNode, false);
+  }
 
-	$('#todo-list')._$addEvent('click', function(_event) {
-		var _$dom = $(_event.target);
-		var _$li = _$dom._$parent('li');
-		switch(_$dom._$attr('class')) {
-			case 'destroy':
-				// 删除对应_$li序号
-				var index = 1;
-				removeItem(index);
-				break;
-		}
-	})
-	$('#todo-input')._$addEvent('keyup', function(_event) {
-		var val = $(this)._$val().trim();
-		if(_event.keyCode === 13 && val) {
-			addItem(val);
-			$(this)._$val('');
-		}
-	})
+  $('#todo-list')._$addEvent('click', function(_event) {
+    var _$dom = $(_event.target);
+    var _$li = _$dom._$parent('li');
+    switch(_$dom._$attr('class')) {
+      case 'destroy':
+        removeItem(_$li._$dataset('index'));
+        break;
+    }
+  })
+  $('#todo-input')._$addEvent('keyup', function(_event) {
+    var val = $(this)._$val().trim();
+    if(_event.keyCode === 13 && val) {
+      addItem(val);
+      $(this)._$val('');
+    }
+  })
 });

4e4a489fdfb3a7b6dad0aa5c801d5ebced6ac26f
XiaFan <506868247@qq.com>
2018-03-22 03:24:19 +0800

    还剩下一个过滤器

--- project/todomvc-app-template/js/app.js
+++ project/todomvc-app-template/js/app.js
@@ -5,7 +5,13 @@ NEJ.define([
 ], function($, _e, _u) {
   var todoList = [];
   var listType = 'all';
-  var _$input = null;
+  var _$todoInput = $('#todo-input');
+  var _$list = $('#todo-list');
+  var _$toggleAllInput = $('#toggle-all');
+  var _$toggleAll = $('#toggle-all-label');
+  var _$clearBtn = $('#clearBtn');
+  var ENTER = 13;
+  var ESC = 27;
 
   function render() {
     renderList();
@@ -16,24 +22,31 @@ NEJ.define([
     return (item.isEditing ? 'editing' : '') + (item.isCompleted ? 'completed' : '');
   }
 
+  function getCheckboxStatus(item) {
+    return item.isCompleted ? 'checked' : '';
+  }
+
   function renderList() {
     //  根据type和list来渲染列表
     var ulInnerHTML = '';
     _u._$forEach(todoList, function(_item, _index) {
       ulInnerHTML += '<li class="' + getItemClass(_item) + '" data-index=' + _index + '>' +
         '<div class="view">' +
-        '<input class="toggle" type="checkbox">' +
+        '<input class="toggle" type="checkbox"' + getCheckboxStatus(_item) + '>' +
         '<label class="content">' + _item.value + '</label>' +
         '<button class="destroy"></button>' +
         '</div>' +
         '<input class="edit" value="' + _item.value + '">' +
         '</li>';
     });
-    $('#todo-list')._$html(ulInnerHTML);
+    _$list._$html(ulInnerHTML);
   }
 
   function renderFooter() {
-    $('#num')._$text(todoList.length);
+    var leftItems = todoList.filter(function(item) {
+      return !item.isCompleted;
+    });
+    $('#num')._$text(leftItems.length);
     $('#footer')._$style('display', todoList.length ? 'block' : 'none');
   }
 
@@ -56,8 +69,24 @@ NEJ.define([
 
   // 编辑
   function edit(index) {
-    todoList[index].isEditing = true;
+    var item = todoList[index];
+    if(item.isCompleted) return;
+    item.isEditing = true;
     render();
+  }
+
+  // 切换完成状态
+  function toggleCompleted(index) {
+    todoList[index].isCompleted = !todoList[index].isCompleted;
+    isAllCompleted() ? _$toggleAllInput[0].checked = true : _$toggleAllInput[0].checked = false;
+    render();
+  }
+
+  // 是否所有item都已完成
+  function isAllCompleted() {
+    return todoList.every(function(item) {
+      return item.isCompleted;
+    })
   }
 
   // 保存
@@ -71,12 +100,56 @@ NEJ.define([
     }
   }
 
-  // list事件绑定
-  $('#todo-list')._$addEvent('click dblclick', function(_event) {
+  function triggerSave(index) {
+    var _$input = _$list._$children('li[data-index=' + index + ']')._$children('input');
+    var oldValue = _$input._$val();
+    _$input[0].focus();
+    _$input._$addEvent('blur keyup', function(_event) {
+      switch(_event.type) {
+        case 'blur':
+          $(this)._$clearEvent();
+          save(index, $(this)._$val());
+          break;
+        case 'keyup':
+          if(_event.keyCode === ENTER) {
+            $(this)._$clearEvent();
+            save(index, $(this)._$val());
+          }
+          if(_event.keyCode === ESC) {
+            $(this)._$clearEvent();
+            save(index, oldValue);
+          }
+          break;
+      }
+    });
+
+  }
+
+  _$todoInput._$addEvent('keyup', function(_event) {
+    var val = $(this)._$val().trim();
+    if(_event.keyCode === ENTER && val) {
+      addItem(val);
+      $(this)._$val('');
+    }
+  })
+  _$toggleAll._$addEvent('click', function() {
+    var flag = isAllCompleted();
+    if(flag) {
+      todoList.forEach(function(item) {
+        item.isCompleted = false;
+      })
+    } else {
+      todoList.forEach(function(item) {
+        item.isCompleted = true;
+      })
+    }
+    render();
+  })
+  // list事件代理
+  _$list._$addEvent('click dblclick', function(_event) {
     var _$dom = $(_event.target);
     var _$li = _$dom._$parent('li');
     var _$index = _$li._$dataset('index');
-    console.log(_event.type);
     switch(_$dom._$attr('class')) {
       case 'destroy':
         removeItem(_$index);
@@ -84,24 +157,18 @@ NEJ.define([
       case 'content':
         if(_event.type === 'dblclick') {
           edit(_$index);
-          // todo 在input上绑定保存事件
-          _$input = _$dom._$parent('div')._$siblings('input');
-          setTimeout(function() {
-            console.log(_$input[0]);
-            _$input[0].id = 'xxxxxx';
-          }, 2000);
+          triggerSave(_$index);
         }
         break;
-      // case 'edit':
-      //    save(_$index, _$dom._$val());
-      //   break;
+      case 'toggle':
+        toggleCompleted(_$index);
     }
   })
-  $('#todo-input')._$addEvent('keyup', function(_event) {
-    var val = $(this)._$val().trim();
-    if(_event.keyCode === 13 && val) {
-      addItem(val);
-      $(this)._$val('');
-    }
+  _$clearBtn._$addEvent('click', function() {
+    todoList = todoList.filter(function(item) {
+      return !item.isCompleted;
+    });
+    render();
   })
 });
+
